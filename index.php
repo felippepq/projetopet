b1251cfd667b9f2268a78cc7c8342171f80e097a
Felippe <felippe228@gmail.com>
2019-10-17 16:44:28 -0300

    Adicionado Bootstrap

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <!DOCTYPE html>
-<html lang="pt-be">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="bootstrap/css">
+    <link rel="stylesheet" href="css/estilo.css">
     <title>Pagina Inicial</title>
 </head>
         <body>
             
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>   
         </body>
 </html>
